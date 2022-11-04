80fc0c41f101f871724b543870763859d9eef038
hhmadan <hhmadan03@gmail.com>
2022-11-04 13:27:33 +0530

    Initial Commit

---
+++ src/hashtableandbst/BinarySearchTreeClass.java
@@ -0,0 +1,7 @@
+package hashtableandbst;
+
+public class BinarySearchTreeClass {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO HASHTABLE AND BINARY SEARCH TREE PROGRAMS");
+    }
+}
