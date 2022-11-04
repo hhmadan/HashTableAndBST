68b9d6906e2d9cd75f232bea000d467d41c56598
hhmadan <hhmadan03@gmail.com>
2022-11-04 23:00:03 +0530

    ADDED: UC2_Find Frequency of Words In given Paragraph

--- src/hashtableandbst/HashTableClass.java
+++ src/hashtableandbst/HashTableClass.java
@@ -28,10 +28,11 @@ public class HashTableClass {
         }
         public void getHashTable(String string, HashTableClass hT){
             int timesVisited = -1;
-            int freq = 1;
+            int freq;
             String[] storeWords = string.toLowerCase().split(" ");
             int[]count = new int[storeWords.length];
             for(int i =0; i< storeWords.length; i++){
+                freq =1;
                 for (int j = i + 1; j < count.length; j++){
                     if (storeWords[i].equals(storeWords[j]))
                     {
@@ -56,7 +57,8 @@ public class HashTableClass {
                 System.out.println("HashTable is empty");
             }
             else{
-                System.out.println("Frequency of Words in given Statement");
+                System.out.println("Frequency of Words in given Paragraph:\nParanoids are not paranoid because they are " +
+                        "paranoid but because they\n keep putting themselves deliberately into paranoid avoidable situations");
                 while (current != null) {
                     System.out.println(current.key + "==>" + current.value);
                     current = current.next;
@@ -66,7 +68,8 @@ public class HashTableClass {
 
     public static void main(String[] args) {
         HashTableClass hTable = new HashTableClass();
-        String statement = "To be or not to be";
+        String statement = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves" +
+                " deliberately into paranoid avoidable situations";
         hTable.getHashTable(statement,hTable);
         hTable.displayHT();
     }
